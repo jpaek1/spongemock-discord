4ac3068641d6cebdfe0468fbf1197f5cd910af77
jpaek1 <jyoyopaek@gmail.com>
2021-05-25 14:11:55 -0400

    connected

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-const Discord = require("discord");
-const client = new Discord.Client();
+const { Client, Intents } = require("discord.js");
+
+const myIntents = new Intents();
+myIntents.add("GUILD_MESSAGE_REACTIONS");
+const client = new Client({ intents: myIntents });
 
 const keys = require("./config/keys");
 
 client.on("ready", () => {
   console.log("i'M ReaDy tO lIstEN");
+});
+
+client.on("messageReactionAdd", (messageReaction) => {
+  // check for emoji.id === null to see if valid when setting
+  console.log(messageReaction.emoji.name);
+  console.log(client.emojis);
 });
 
 client.login(keys.botToken);
